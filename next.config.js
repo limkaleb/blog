f21a7684c8d1bdb0f80771b47a880a21793b41dc
Kaleb Lim <limkaleb@gmail.com>
2023-07-30 18:48:48 +0700

    fix some paths

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   basePath: '/blog',
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
+  trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
 }
