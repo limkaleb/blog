4c244949bdeef8c648c5a22dc5437c3189686931
Kaleb Lim <limkaleb@gmail.com>
2023-07-30 18:41:39 +0700

    fix basePath for github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/blog',
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
   // trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
