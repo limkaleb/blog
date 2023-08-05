633602b24f8e5257a7ac6d1c2e2fa52eff25e235
Kaleb Lim <kaleb@pickupp.io>
2023-08-05 18:03:52 +0700

    update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,19 +1,42 @@
 export default function Footer() {
   return (
-    <div className="mt-8 border-t border-gray-500">
-      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="relative h-48 flex flex-col justify-center items-start gap-8">
-          <div className="flex gap-8">
-            <h1>This is FOOTER 1</h1>
-            <h1>This is FOOTER 2</h1>
-            <h1>This is FOOTER 3</h1>
+    <div className="mt-8 border-t border-gray-600">
+      <div className="relative h-48">
+        <footer>
+          <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+            <p className="text-md sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
+              This blog is primarily for my own personal use.
+            </p>
+            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+              <a className="text-gray-600" href="https://www.github.com/limkaleb" target="_blank" rel="noopener noreferrer"></a>
+                <svg fill="currentColor" viewBox="0 0 24 24"  stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5">
+                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                </svg>
+              <a className="ml-3" href="https://www.facebook.com/limkaleb" target="_blank" rel="noopener noreferrer">
+                <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a className="ml-3" href="https://twitter.com/limkaleb1" target="_blank" rel="noopener noreferrer">
+                <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a className="ml-3" href="https://www.instagram.com/limkaleb/" target="_blank" rel="noopener noreferrer">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a>
+              <a className="ml-3" href="https://www.linkedin.com/in/limkaleb/" target="_blank" rel="noopener noreferrer">
+                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+                  <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                </svg>
+              </a>
+            </span>
           </div>
-          <div className="flex gap-8">
-            <h1>This is FOOTER 4</h1>
-            <h1>This is FOOTER 5</h1>
-            <h1>This is FOOTER 6</h1>
-          </div>
-      </div>
+        </footer>
       </div>
     </div>
   )
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex justify-between h-16">
         <div className="flex justify-between items-center w-full">
-          <Link href="/">
+          <Link href="/" className="hover:underline">
             <h1 className="text-2xl font-medium">Soli Deo Gloria</h1>
           </Link>
           <ThemeButton />
