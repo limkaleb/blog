ce7c926f1ccc231cdabdbe6c665ec329b61fb8ce
Kaleb Lim <kaleb@pickupp.io>
2024-01-20 17:43:23 +0700

    add --turbo to next dev script

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/blog',
-  assetPrefix: 'https://limkaleb.github.io/blog/',
+  // basePath: '/blog',
+  // assetPrefix: 'https://limkaleb.github.io/blog/',
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
   // trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
